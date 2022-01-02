8cd954345cd4eaad462dbebea5fca56116649fb9
Ho3ein <hos.kh93@gmail.com>
2022-01-02 12:43:10 +0330

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.component";
+
+import { SearchBox } from "./components/search-box/search-box.component"
+
+class App extends Component{
+
+  constructor(){
+    super();
+
+    this.state = {
+      robots : [],
+      searchField: ''
+    };
+
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({robots: users}))
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
+
+  render() {
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter( monster => 
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()) 
+    );
+
+    return(
+      <div className="App">
+        <h1> Robots Rolodex </h1>
+        <SearchBox 
+          placeholder="Search robots...."
+          handleChange={this.handleChange}
+        />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './search-box.styles.css'
+
+export const SearchBox = (props) => {
+    return(
+        <input
+            type="search"
+            className="search"
+            placeholder={props.placeholder} 
+            onChange={props.handleChange} 
+        />
+    )
+}
